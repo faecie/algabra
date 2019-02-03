170a2556bdf2e306c613b8f5f43714a675dcb43f
Ruslan Umyarov <umyarovrr@gmail.com>
2019-02-03 16:53:05 +1100

    Add Fibonacci heap data-structure
    Fibonacci heaps are awesome. They expose the same interface as a regular
    heaps but emit constant amortized time performance on insert and decrease
    operations, along with constant-time union operation. It brings huge
    performance boost for some formerly long running algorithms. However the
    implementation is very convoluted. Kudos for its developers - Fredman
    and Tarjan
    
    **What else in here**
    - [x] Implement 'Insert' for regular heaps
    - [x] Implement 'Maximumm' for regular heaps
    - [x] Implement 'Extract','Increase/decrease' for regular heaps
    - General interface for all heaps
    - Better 'pythonic' approach on generating random samples
    
    Implementation was borrowed from [Cormen's book](http://mitpress.mit.edu/books/introduction-algorithms-third-edition)

--- algabra/datastructures/heap.py
+++ algabra/datastructures/heap.py
@@ -1,12 +1,48 @@
+from __future__ import annotations
+
 import abc
 import math
 import typing
 
+import scipy.constants
 
-class Heap(abc.ABC):
-    __slots__ = ['_heap', '_heap_size']
 
-    def __init__(self, input_data: typing.Iterable) -> None:
+class AbstractHeap(abc.ABC):
+
+    @abc.abstractmethod
+    def get_highest(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    def extract_highest(self):
+        pass
+
+    @abc.abstractmethod
+    def promote_key(self, element, new_value: int) -> None:
+        old_value = self.get_value(element)
+        if not self._is_higher(new_value, old_value):
+            raise TypeError(
+                'New value %d must be higher in a priority than old value %d' %
+                (new_value, old_value))
+
+    @abc.abstractmethod
+    def insert(self, value) -> None:
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def _is_higher(cls, one, other) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def get_value(self, item):
+        pass
+
+
+class Heap(AbstractHeap):
+    __slots__ = '_heap', '_heap_size'
+
+    def __init__(self, input_data: typing.Iterable[int]) -> None:
         self._heap = list(input_data)
         self._heap_size = len(self._heap)
 
@@ -26,7 +62,7 @@ class Heap(abc.ABC):
         return 2 * index + 2
 
     @classmethod
-    def get_sorted_list(cls, unsorted_list: typing.Iterable) -> list:
+    def get_sorted_list(cls, unsorted_list: typing.Iterable[int]) -> list:
         inner_heap = cls(unsorted_list)
         for index in range(len(inner_heap._heap) - 1, 0, -1):
             inner_heap._swap(0, index)
@@ -35,9 +71,38 @@ class Heap(abc.ABC):
 
         return inner_heap._heap
 
-    @abc.abstractmethod
-    def _is_higher(self, one: int, other: int) -> bool:
-        pass
+    def get_highest(self) -> int:
+        return self._heap[0]
+
+    def extract_highest(self) -> typing.Optional[int]:
+        if self._heap_size < 1:
+            return None
+        result = self._heap[0]
+        self._heap[0] = self._heap[self._heap_size - 1]
+        self._heap_size -= 1
+        self._heapify(0)
+
+        return result
+
+    def promote_key(self, index: int, new_value: int) -> None:
+        super().promote_key(index, new_value)
+
+        self._heap[index] = new_value
+        self._sift_up(index)
+
+    def insert(self, value: int) -> None:
+        self._heap_size += 1
+        self._heap.append(value)
+        self._sift_up(self._heap_size - 1)
+
+    def get_value(self, item: int) -> int:
+        return self._heap[item]
+
+    def _sift_up(self, index: int) -> None:
+        while index > 0 and not self._index_higher(self._parent(index), index):
+            parent = self._parent(index)
+            self._swap(parent, index)
+            index = parent
 
     def _heapify(self, index: int) -> None:
         iterate = True
@@ -46,10 +111,10 @@ class Heap(abc.ABC):
             right = self._right(index)
             highest = index
 
-            if self._in_heap(left) and self._is_higher(left, index):
+            if self._in_heap(left) and self._index_higher(left, index):
                 highest = left
 
-            if self._in_heap(right) and self._is_higher(right, highest):
+            if self._in_heap(right) and self._index_higher(right, highest):
                 highest = right
 
             if highest != index:
@@ -57,6 +122,9 @@ class Heap(abc.ABC):
                 index = highest
             else:
                 iterate = False
+
+    def _index_higher(self, one: int, other: int) -> bool:
+        return self._is_higher(self._heap[one], self._heap[other])
 
     def _in_heap(self, index: int) -> bool:
         return index < self._heap_size
@@ -69,11 +137,314 @@ class Heap(abc.ABC):
 
 class MaxHeap(Heap):
 
-    def _is_higher(self, one: int, other: int) -> bool:
-        return self._heap[one] > self._heap[other]
+    @classmethod
+    def _is_higher(cls, one: int, other: int) -> bool:
+        return one > other
 
 
 class MinHeap(Heap):
 
-    def _is_higher(self, one: int, other: int) -> bool:
-        return self._heap[one] < self._heap[other]
+    @classmethod
+    def _is_higher(cls, one: int, other: int) -> bool:
+        return one < other
+
+
+class FibonacciHeap(AbstractHeap, typing.Sized):
+    __slots__ = '_highest', '_nodes_count'
+
+    def __init__(self, node: FibonacciHeapNode = None) -> None:
+        self._highest = node
+        self._nodes_count = 0
+
+    def __len__(self) -> int:
+        return self._nodes_count
+
+    @classmethod
+    def _is_higher(cls, one: typing.Union[FibonacciHeapNode, int, None],
+                   other: typing.Union[FibonacciHeapNode, int, None]) -> bool:
+        if one is None or other is None:
+            return False
+
+        one = one._key if isinstance(one, FibonacciHeapNode) else one
+        other = other._key if isinstance(other, FibonacciHeapNode) else other
+
+        return cls._is_value_higher(one, other)
+
+    @staticmethod
+    @abc.abstractmethod
+    def _is_value_higher(one: int, other: int) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def _promote_value(self, key: int, level: typing.Optional[int] = 1) -> int:
+        pass
+
+    @staticmethod
+    def _remove_from_root_list(node: FibonacciHeapNode):
+        node._left._right = node._right
+        node._right._left = node._left
+
+        node._left = node._right = None
+
+    @staticmethod
+    def _exchange_roots(one: FibonacciHeapNode,
+                        other: FibonacciHeapNode) -> None:
+        other_around_one = one._left == other and one._right == other
+        one_around_other = other._left == one and other._right == one
+        if other_around_one != one_around_other:
+            raise Exception(
+                'List is broken on nodes %d and %d' % (one._key, other._key))
+
+        if one == other or other_around_one:
+            return
+
+        one_adjacent_other = one._right == other and other._left == one
+        other_adjacent_one = one._left == other and other._right == one
+        are_adjacent = one_adjacent_other or other_adjacent_one
+
+        if other._right == one:
+            one, other = other, one
+
+        swapper_vector = one._left, other._left, one._right, other._right
+
+        if are_adjacent:
+            one._left = swapper_vector[2]
+            other._left = swapper_vector[0]
+            one._right = swapper_vector[3]
+            other._right = swapper_vector[1]
+        else:
+            one._left = swapper_vector[1]
+            other._left = swapper_vector[0]
+            one._right = swapper_vector[3]
+            other._right = swapper_vector[2]
+
+        one._left._right = one
+        one._right._left = one
+        other._left._right = other
+        other._right._left = other
+
+    @classmethod
+    def union(cls, one: FibonacciHeap, other: FibonacciHeap) -> FibonacciHeap:
+        result = cls()
+        result._highest = one._highest
+
+        result._merge_with_root(other)
+        if cls._is_higher(other._highest, one._highest):
+            result._highest = other._highest
+
+        result._nodes_count = one._nodes_count + other._nodes_count
+
+        return result
+
+    def insert(self, node: FibonacciHeapNode):
+        node._degree = 0
+        node._parent = None
+        node._child = None
+        node._mark = False
+
+        self._insert_in_root_list(node)
+
+        self._nodes_count += 1
+
+    def get_highest(self) -> int:
+        return self._highest._key
+
+    def _extract_highest_node(self) -> typing.Optional[FibonacciHeapNode]:
+        result = self._highest
+        if result is not None:
+            child = result._child
+            terminal = False
+            while child is not None and not terminal:
+                terminal = child._right == child
+                next_child = child._right
+                result._remove_child(child)
+                self._insert_in_root_list(child)
+                child._parent = None
+                child = next_child
+
+            next_root = result._right
+            self._remove_from_root_list(result)
+
+            if result == next_root:
+                self._highest = None
+            else:
+                self._highest = next_root
+                self._consolidate()
+            self._nodes_count -= 1
+
+        return None if result is None else result
+
+    def extract_highest(self) -> typing.Optional[int]:
+        result = self._extract_highest_node()
+
+        return None if result is None else result._key
+
+    def promote_key(self, node: FibonacciHeapNode, new_value: int) -> None:
+        super().promote_key(node, new_value)
+
+        node._key = new_value
+
+        parent = node._parent
+        if parent is not None and self._is_higher(node, parent):
+            node._mark = True
+            self._cut(node)
+
+        if self._is_higher(node, self._highest):
+            self._highest = node
+
+    def delete(self, node: FibonacciHeapNode) -> None:
+        if self._highest is not None:
+            self.promote_key(node, self._promote_value(self._highest._key))
+            self.extract_highest()
+
+    def get_value(self, node: FibonacciHeapNode) -> typing.Optional[int]:
+        return None if node is None else node._key
+
+    def _cut(self, node: FibonacciHeapNode) -> None:
+        target = node
+        parent = target._parent
+        iterate = True
+        while parent and iterate:
+            if target._mark:
+                parent._remove_child(target)
+                self._insert_in_root_list(target)
+                target._mark = False
+                target = parent
+                parent = parent._parent
+            else:
+                target._mark = True
+                iterate = False
+
+    def _consolidate(self) -> None:
+        nodes_per_degree = [None] * self._get_max_degrees_count()
+
+        terminal = False
+        next_node = self._highest
+        while next_node != terminal and next_node is not None:
+            terminal = next_node if not terminal else terminal
+            degree = next_node._degree
+            while nodes_per_degree[degree] is not None:
+                same_degree_node = nodes_per_degree[degree]
+
+                if self._is_higher(same_degree_node, next_node):
+                    if terminal == same_degree_node:
+                        terminal = same_degree_node._right
+                    self._exchange_roots(next_node, same_degree_node)
+                    next_node, same_degree_node = same_degree_node, next_node
+
+                if terminal == same_degree_node:
+                    terminal = same_degree_node._right
+                self._remove_from_root_list(same_degree_node)
+                next_node._add_child(same_degree_node)
+                same_degree_node._mark = False
+
+                nodes_per_degree[degree] = None
+                degree += 1
+
+            nodes_per_degree[degree] = next_node
+            next_node = next_node._right
+
+        self._highest = None
+        for node in nodes_per_degree:
+            if node is not None:
+                self._insert_in_root_list(node)
+
+    def _get_max_degrees_count(self) -> int:
+        return math.floor(math.log(self._nodes_count, scipy.constants.golden))
+
+    def _insert_in_root_list(self, node: FibonacciHeapNode):
+        node._parent = None
+        if self._highest is not None:
+            self._highest.add_neighbour(node)
+            if self._is_higher(node, self._highest):
+                self._highest = node
+        else:
+            self._highest = node
+            node._left = node._right = node
+
+    def _merge_with_root(self, other_root: FibonacciHeap) -> None:
+        if other_root._highest is not None:
+            if self._highest is not None:
+                self_right_neighbor = self._highest._right
+                self._highest._right = other_root._highest._right
+                other_root._highest._right._left = self._highest
+
+                other_root._highest._right = self_right_neighbor
+                self_right_neighbor._left = other_root._highest
+            else:
+                self._highest = other_root._highest
+
+
+class FibonacciHeapNode:
+    __slots__ = [
+        '_degree',
+        '_mark',
+        '_parent',
+        '_child',
+        '_left',
+        '_right',
+        '_key',
+    ]
+
+    def __init__(self,
+                 key: int,
+                 parent: FibonacciHeapNode = None,
+                 child: FibonacciHeapNode = None) -> None:
+        self._left = self._right = self
+        self._key = key
+        self._degree = 0
+        self._mark = False
+        self._child = child
+        self._parent = parent
+
+    def add_neighbour(self, node: FibonacciHeapNode) -> None:
+        node._right, node._left = self._right, self
+
+        self._right._left = node
+        self._right = node
+
+        node._parent = self._parent
+
+    def _remove_child(self, node: FibonacciHeapNode) -> None:
+        if node._parent != self or self._child is None:
+            raise ValueError('%d has not a child %d' % (self._key, node._key))
+
+        if self._child == node and node._right == node._left == node:
+            self._child = None
+        else:
+            node._left._right, node._right._left = node._right, node._left
+            self._child = node._right
+
+        node._right = node._left = node._parent = None
+
+        self._degree -= 1
+
+    def _add_child(self, node: FibonacciHeapNode) -> None:
+        if self._child is None:
+            self._child = node
+            node._right = node._left = node
+            node._parent = self
+        else:
+            self._child.add_neighbour(node)
+
+        self._degree += 1
+
+
+class MinFibonacciHeap(FibonacciHeap):
+
+    @staticmethod
+    def _is_value_higher(one: int, other: int) -> bool:
+        return one < other
+
+    def _promote_value(self, key: int, level: typing.Optional[int] = 1) -> int:
+        return key - level
+
+
+class MaxFibonacciHeap(FibonacciHeap):
+
+    @staticmethod
+    def _is_value_higher(one: int, other: int) -> bool:
+        return one > other
+
+    def _promote_value(self, key: int, level: typing.Optional[int] = 1) -> int:
+        return key + level
--- tests/unit/algabra/datastructures/heap_test.py
+++ tests/unit/algabra/datastructures/heap_test.py
@@ -1,23 +1,203 @@
-import unittest
+import math
 import random
+import unittest
+
 import algabra.datastructures.heap as my_heap
 
 
 class HeapTestCase(unittest.TestCase):
 
-    def test_sorting_asc(self):
+    def test_sorting_asc(self) -> None:
         items_count = 100
         sorted_list = list(range(items_count))
         unsorted_list = random.sample(sorted_list, items_count)
 
         assert my_heap.MaxHeap.get_sorted_list(unsorted_list) == sorted_list
 
-    def test_sorting_desc(self):
+    def test_sorting_desc(self) -> None:
         items_count = 100
         sorted_list = list(range(items_count, 0, -1))
         unsorted_list = random.sample(sorted_list, items_count)
 
         assert my_heap.MinHeap.get_sorted_list(unsorted_list) == sorted_list
+
+    def test_sort_extracting_desc(self) -> None:
+        items_count = 100
+        sorted_list = list(range(items_count, 0, -1))
+        max_heap = my_heap.MaxHeap(random.sample(sorted_list, items_count))
+        target_list = _get_sorted_by_extracting(max_heap)
+
+        assert target_list == sorted_list
+
+    def test_sort_extracting_asc(self) -> None:
+        items_count = 100
+        sorted_list = list(range(items_count))
+        min_heap = my_heap.MinHeap(random.sample(sorted_list, items_count))
+        target_list = _get_sorted_by_extracting(min_heap)
+
+        assert target_list == sorted_list
+
+    def test_increase_key(self) -> None:
+        items_count = 100
+        max_heap = my_heap.MaxHeap(
+            random.sample(list(range(items_count)), items_count))
+
+        max_heap.promote_key(93, 60)
+        assert max_heap.extract_highest() == 99
+
+        max_heap.promote_key(11, 100)
+        assert max_heap.extract_highest() == 100
+
+    def test_decrease_key(self) -> None:
+        items_count = 100
+        min_heap = my_heap.MinHeap(
+            random.sample(list(range(items_count)), items_count))
+
+        min_heap.promote_key(85, 28)
+        assert min_heap.extract_highest() == 0
+
+        min_heap.promote_key(66, -1)
+        assert min_heap.extract_highest() == -1
+
+    def test_demote_fails(self) -> None:
+        items_count = 100
+        min_heap = my_heap.MinHeap(
+            random.sample(list(range(items_count)), items_count))
+
+        with self.assertRaises(TypeError):
+            min_heap.promote_key(44, 100)
+
+    def test_create_inserting(self) -> None:
+        items_count = 100
+        max_heap = my_heap.MaxHeap([])
+        for value in random.sample(list(range(items_count)), items_count):
+            max_heap.insert(value)
+
+        result = _get_sorted_by_extracting(max_heap)
+        assert all(result[i] > result[i + 1] for i in range(len(result) - 1))
+
+    def test_inserting(self) -> None:
+        items_count = 100
+        max_heap = my_heap.MaxHeap(
+            random.sample(list(range(items_count)), items_count))
+
+        max_heap.insert(101)
+        assert max_heap.get_highest() == 101
+
+        max_heap.insert(100)
+        assert max_heap.get_highest() == 101
+
+        max_heap.extract_highest()
+        assert max_heap.get_highest() == 100
+
+
+class FibonacciHeapTestCase(unittest.TestCase):
+
+    def test_empty_heap(self) -> None:
+        heap = my_heap.MaxFibonacciHeap()
+        assert not heap.extract_highest()
+
+    def test_sort_desc(self) -> None:
+        heap = my_heap.MaxFibonacciHeap()
+        items_count = 100
+        for value in random.sample(list(range(items_count)), items_count):
+            heap.insert(my_heap.FibonacciHeapNode(value))
+
+        target = _get_sorted_by_extracting(heap)
+        assert all(target[i] > target[i + 1] for i in range(len(target) - 1))
+
+    def test_sort_asc(self) -> None:
+        heap = my_heap.MinFibonacciHeap()
+        items_count = 100
+        for value in random.sample(list(range(items_count)), items_count):
+            heap.insert(my_heap.FibonacciHeapNode(value))
+
+        target = _get_sorted_by_extracting(heap)
+        assert all(target[i] < target[i + 1] for i in range(len(target) - 1))
+
+    def test_union(self) -> None:
+        heap1 = my_heap.MinFibonacciHeap()
+        heap2 = my_heap.MinFibonacciHeap()
+        items_count = 100
+
+        count = 0
+        for value in random.sample(list(range(items_count)), items_count):
+            if count < items_count / 2:
+                heap1.insert(my_heap.FibonacciHeapNode(value))
+            else:
+                heap2.insert(my_heap.FibonacciHeapNode(value))
+            count += 1
+
+        target_heap = my_heap.MinFibonacciHeap.union(heap1, heap2)
+        assert len(target_heap) == 100
+
+        target_list = _get_sorted_by_extracting(target_heap)
+        assert len(target_list) == 100
+
+        assert all(target_list[i] < target_list[i + 1] for i in range(100 - 1))
+
+    def test_decrease(self) -> None:
+        heap = my_heap.MinFibonacciHeap()
+        items_count = 1000
+        self._extract_promote_sequences(heap, items_count)
+
+        target = _get_sorted_by_extracting(heap)
+        for i in range(len(target) - 1):
+            if target[i] > target[i + 1]:
+                msg = 'Value %d is bigger than %s' % (target[i], target[i + 1])
+                self.fail(msg)
+
+    def test_increase(self) -> None:
+        heap = my_heap.MaxFibonacciHeap()
+        items_count = 1000
+        self._extract_promote_sequences(heap, items_count)
+
+        target = _get_sorted_by_extracting(heap)
+        for i in range(len(target) - 1):
+            if target[i] < target[i + 1]:
+                msg = 'Value %d is less than %s' % (target[i], target[i + 1])
+                self.fail(msg)
+
+    def test_delete(self) -> None:
+        heap = my_heap.MaxFibonacciHeap()
+        items_count = 1000
+        values = random.sample(list(range(items_count)), items_count)
+        nodes_collection = [my_heap.FibonacciHeapNode(i) for i in values]
+        for node in nodes_collection:
+            heap.insert(node)
+
+        target_index = math.floor(items_count / random.randint(0, items_count))
+        target_node = nodes_collection[target_index]
+        heap.delete(target_node)
+
+        target_list = _get_sorted_by_extracting(heap)
+        assert target_list.count(target_node._key) == 0
+
+    def _extract_promote_sequences(self, heap, items_count):
+        values = random.sample(list(range(items_count)), items_count)
+        nodes_collection = [my_heap.FibonacciHeapNode(i) for i in values]
+        for node in nodes_collection:
+            heap.insert(node)
+        extract = True
+        extracted_nodes = list()
+        for node in nodes_collection:
+            if extract:
+                extracted = heap._extract_highest_node()
+                extracted_nodes.append(extracted)
+                extract = False
+            elif extracted_nodes.count(node) == 0:
+                level = random.randint(1, items_count)
+                heap.promote_key(node, heap._promote_value(node._key, level))
+                extract = True
+
+
+def _get_sorted_by_extracting(heap: my_heap.AbstractHeap) -> list:
+    target_list = []
+    max_item = heap.extract_highest()
+    while max_item is not None:
+        target_list.append(max_item)
+        max_item = heap.extract_highest()
+    return target_list
 
 
 if __name__ == '__main__':
